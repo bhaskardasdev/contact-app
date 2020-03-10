e89a7cb5b51730333f5e696999c247910acf9451
bhaskrdas090 <bhaskardas090@gmail.com>
2020-03-10 19:12:03 +0530

    Updates

---
+++ precache-manifest.90347b7f6b098a5deec1c59e5308958e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43346dd423ff135768fbc606b98069a",
+    "url": "/contact-app/index.html"
+  },
+  {
+    "revision": "7b6d557563d683ff2fb2",
+    "url": "/contact-app/static/css/main.8ed31cbd.chunk.css"
+  },
+  {
+    "revision": "ee8e8496c4735b1cbc2d",
+    "url": "/contact-app/static/js/2.bdbce67b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/contact-app/static/js/2.bdbce67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b6d557563d683ff2fb2",
+    "url": "/contact-app/static/js/main.c871ce85.chunk.js"
+  },
+  {
+    "revision": "022846a7470257b4df5f",
+    "url": "/contact-app/static/js/runtime-main.d43a9df4.js"
+  }
+]);
